6090b86674799c2e369ec7b34c26a9a82575e760
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-11-16 10:56:45 -0300

    Implementação DateUtil - Verificação de Logs

--- src/main/java/com/guilhermeoscp/apisistemaescolar/ApiSistemaEscolarApplication.java
+++ src/main/java/com/guilhermeoscp/apisistemaescolar/ApiSistemaEscolarApplication.java
@@ -1,13 +1,9 @@
 package com.guilhermeoscp.apisistemaescolar;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.guilhermeoscp.apisistemaescolar.endpoint")
-@SpringBootApplication
+@SpringBootApplication //@Configuration @EnableAutoConfiguration @ComponentScan
 public class ApiSistemaEscolarApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/guilhermeoscp/apisistemaescolar/endpoint/StudentEndpoint.java
+++ src/main/java/com/guilhermeoscp/apisistemaescolar/endpoint/StudentEndpoint.java
@@ -1,20 +1,27 @@
 package com.guilhermeoscp.apisistemaescolar.endpoint;
 
-import java.util.List;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilhermeoscp.apisistemaescolar.model.Student;
+import com.guilhermeoscp.apisistemaescolar.util.DateUtil;
 
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
 	
+	@Autowired
+	private DateUtil dateUtil;
+	
 	@RequestMapping(method = RequestMethod.GET,path = "/list")
 	public List<Student> listAll () {
-		return Arrays.asList(new Student("Jonathan"), new Student("Joseph"));
+		System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+		return Arrays.asList(new Student("Jonathan"), new Student("Joseph"),new Student("Jotaro"),new Student("Josuke"),new Student("Giorno"),new Student("Jolyne"),new Student("Johnny"));
 	}
 }
