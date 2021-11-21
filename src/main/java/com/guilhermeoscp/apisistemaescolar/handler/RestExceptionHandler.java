f5a9231fe6a23fd7cfe836fe9b1d29d2c0603282
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-11-21 09:52:01 -0300

    Implementação Tratativa de Erros REST - Mensagem Padronizada para Student Not Found

---
+++ src/main/java/com/guilhermeoscp/apisistemaescolar/handler/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.guilhermeoscp.apisistemaescolar.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.guilhermeoscp.apisistemaescolar.error.ResourceNotFoundDetails;
+import com.guilhermeoscp.apisistemaescolar.error.ResourceNotFoundException;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rfnException) {
+		ResourceNotFoundDetails rfnDetails = ResourceNotFoundDetails.Builder
+				.newBuilder()
+				.timestamp(new Date().getTime())
+				.status(HttpStatus.NOT_FOUND.value())
+				.title("Resource not Found")
+				.detail(rfnException.getMessage())
+				.developerMessage(rfnException.getClass().getName())
+				.build();
+		return new ResponseEntity<>(rfnDetails, HttpStatus.NOT_FOUND);		
+	}
+
+}
