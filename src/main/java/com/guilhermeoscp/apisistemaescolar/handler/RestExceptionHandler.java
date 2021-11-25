d2222ec20e9c4c20207bf1042703d3138cb67473
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-11-25 17:29:09 -0300

    Implementação Tratativa de Erros REST - Property Reference Exception

--- src/main/java/com/guilhermeoscp/apisistemaescolar/handler/RestExceptionHandler.java
+++ src/main/java/com/guilhermeoscp/apisistemaescolar/handler/RestExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -66,4 +67,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 				.builder();
 		return new ResponseEntity<>(errorDetails, headers, status);
 	}
+	
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<?> handlePropertyReferenceException(PropertyReferenceException ex) {
+        ResourceNotFoundDetails resourceNotFoundDetails = ResourceNotFoundDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .title("Internal Server Error")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .builder();
+
+        return new ResponseEntity<>(resourceNotFoundDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }	
 }
