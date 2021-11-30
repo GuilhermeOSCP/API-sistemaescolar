d7fbc2ea971c2b3c507a0c1bb518a6b925d72554
Guilherme Pimentel <guilhermeoscp@gmail.com>
2021-11-30 10:38:09 -0300

    Implementação Client Spring Com RestTemplate - POST

---
+++ src/main/java/com/guilhermeoscp/apisistemaescolar/javaclient/JavaClientDAO.java
@@ -0,0 +1,47 @@
+package com.guilhermeoscp.apisistemaescolar.javaclient;
+
+import java.util.List;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.guilhermeoscp.apisistemaescolar.model.PageableResponse;
+import com.guilhermeoscp.apisistemaescolar.model.Student;
+
+public class JavaClientDAO {
+	
+	private RestTemplate restTemplate = new RestTemplateBuilder()
+			.rootUri("http://localhost:8080/v1/protected/students")
+			.basicAuthentication("admin", "St@ndM@st3r").build();
+	
+	private RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+			.rootUri("http://localhost:8080/v1/protected/students")
+			.basicAuthentication("admin", "St@ndM@st3r").build();
+	
+	public Student findById(long id) {
+		return restTemplate.getForObject("/{id}", Student.class, id);
+	}
+	
+	public List<Student> listAll() {
+		ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET,null, 
+				new ParameterizedTypeReference<PageableResponse<Student>>() {});
+		return exchange.getBody().getContent();
+	}
+	
+	public Student save(Student student) {
+		ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/", HttpMethod.POST,new HttpEntity<>(student,createJSONHeader()), Student.class);
+		return exchangePost.getBody();
+	}
+	
+	private static HttpHeaders createJSONHeader() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		return headers;
+	}
+}
--- src/main/java/com/guilhermeoscp/apisistemaescolar/javaclient/JavaSpringClientTest.java
+++ src/main/java/com/guilhermeoscp/apisistemaescolar/javaclient/JavaSpringClientTest.java
@@ -1,38 +1,21 @@
 package com.guilhermeoscp.apisistemaescolar.javaclient;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import java.util.List;
 
-import com.guilhermeoscp.apisistemaescolar.model.PageableResponse;
 import com.guilhermeoscp.apisistemaescolar.model.Student;
 
 public class JavaSpringClientTest {
 	
 	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplateBuilder()
-				.rootUri("http://localhost:8080/v1/protected/students")
-				.basicAuthentication("admin", "St@ndM@st3r").build();
-		Student student = restTemplate.getForObject("/{id}", Student.class, 1);
-		System.out.println(student);
-		System.out.println("--------------------");
-		
-		ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 1);
-		System.out.println(forEntity.getBody());
-		System.out.println("--------------------");
-		
-/*		Student[] students = restTemplate.getForObject("/", Student[].class);
-		System.out.println(Arrays.toString(students));
-		System.out.println("--------------------"); 
-		
-		ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET,null, 
-					new ParameterizedTypeReference<List<Student>>() {});
-		System.out.println(exchange.getBody()); */
-		
-		ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET,null, 
-				new ParameterizedTypeReference<PageableResponse<Student>>() {});
-		System.out.println(exchange);
+	
+		Student studentPost = new Student();
+		studentPost.setName("Speedwagon");
+		studentPost.setEmail("speedwagon@gmail.com");
+		JavaClientDAO dao = new JavaClientDAO();
+// 		System.out.println(dao.findById(1));
+		List<Student> students = dao.listAll();
+		System.out.println(students);
+//		System.out.println(dao.save(studentPost));
 	}
+	
 }
